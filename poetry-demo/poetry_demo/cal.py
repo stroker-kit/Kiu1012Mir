c04a686a84f115fbfff8763fd1e5eab87c1391d3
stroker-kit <Patrol-master@mail.ru>
2022-12-13 10:09:15 +0000

    111

---
+++ poetry-demo/poetry_demo/cal.py
@@ -0,0 +1,26 @@
+def main():
+    import random
+    print("what is the result of calculation?")
+    x=random.randit(0,100)
+    y=random.randit(0,100)
+    z=random.randit(0,2)
+    t=0
+    k=0
+    while k<=2:
+        k=+1
+        u=int(input("Введи число"))
+                if z==0:
+                    t=x*y
+                    print(x+"*"+y)
+                elif z==1
+                    print(x+"+"+y)
+                    t=x+y
+                elif z==2
+                    (x+"-"+y)
+                    t=x-y
+            if t=u:
+                print("Correct!")
+            else:
+                print ("lose!")
+                break
+main()
