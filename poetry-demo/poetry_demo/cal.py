f865308eedd08b803ee50b00c8a413e98cbddee2
stroker-kit <Patrol-master@mail.ru>
2022-12-13 10:09:19 +0000

    11

--- poetry-demo/poetry_demo/cal.py
+++ poetry-demo/poetry_demo/cal.py
@@ -1,26 +1,28 @@
+import random
+from random import randint
 def main():
-    import random
     print("what is the result of calculation?")
-    x=random.randit(0,100)
-    y=random.randit(0,100)
-    z=random.randit(0,2)
+    x=random.randint(0,100)
+    y=random.randint(0,100)
+    z=random.randint(0,2)
+    print(x,y)
     t=0
     k=0
     while k<=2:
         k=+1
         u=int(input("Введи число"))
-                if z==0:
-                    t=x*y
-                    print(x+"*"+y)
-                elif z==1
-                    print(x+"+"+y)
-                    t=x+y
-                elif z==2
-                    (x+"-"+y)
-                    t=x-y
-            if t=u:
-                print("Correct!")
-            else:
-                print ("lose!")
-                break
+        if z==0:
+            t=x*y
+            print(x+"*"+y)
+        elif z==1:
+            print(x+"+"+y)
+            t=x+y
+        elif z==2:
+            (x+"-"+y)
+            t=x-y
+        if t==u:
+            print("Correct!")
+        else:
+            print ("lose!")
+            break
 main()
