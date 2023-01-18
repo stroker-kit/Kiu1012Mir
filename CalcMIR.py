893c570b82e8f894282c294477b92bcce51d6e01
stroker-kit <114421648+stroker-kit@users.noreply.github.com>
2023-01-18 10:01:35 +0300

    Update CalcMIR.py

--- CalcMIR.py
+++ CalcMIR.py
@@ -45,7 +45,7 @@ def main():
 
 
         window = tk.Tk()
-        window.geometry("270x230")
+        window.geometry("230x270")
         window.title("Калькулятор")
         calc=tk.Entry(window,justify=RIGHT)
         calc.grid(row=0,column=1,columnspan=4)
